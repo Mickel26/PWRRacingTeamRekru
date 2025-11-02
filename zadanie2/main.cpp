0411ded4421c183f8ef726d0bda19cfa71010bc3
Mickel26 <mikolajfilka@gmail.com>
2025-11-02 22:24:00 +0100

    Changed vector to a dynamic table

--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -4,77 +4,84 @@
 #include <string>
 #include <vector>
 
-std::vector<std::pair<double, double>> Read();
+struct NearestPoints {
+    std::pair<double, double> point1;
+    std::pair<double, double> point2;
+};
+
+bool Read(double (*&points)[2], unsigned int &n);
 std::vector<std::pair<double, double>> findHalo(std::vector<std::pair<double, double>> data);
-std::pair<std::pair<double, double>, double> findNearestPoints(std::vector<std::pair<double, double>> data);
+NearestPoints findNearestPoints(double (*points)[2], unsigned int n);
 
 int main() {
-    std::vector<std::pair<double, double>> data = Read();
+    double (*data)[2] = nullptr;
+    unsigned int size = 0;
 
-    /*for (const auto [x, y] : data) {
-        std:: cout << x << ' ' << y << std::endl;
-    }*/
+    if (!Read(data, size)) {
+        return 1;
+    }
 
-    findHalo(data);
-    findNearestPoints(data);
+    findNearestPoints(data, size);
 
+    delete[] data;
+    data = nullptr;
     return 0;
 }
 
-std::vector<std::pair<double, double>> Read() {
+bool Read(double (*&points)[2], unsigned int &n){
     std::cout << "Enter filename: ";
-    std::string filename = "";
+    std::string filename;
     std::cin >> filename;
 
     std::ifstream file(filename);
-    int n = 0;
-    std::vector<std::pair<double, double>> data;
 
     if (file.is_open()) {
         file >> n;
-        data.reserve(n);
+
+        points = new double[n][2];
 
         double x,y;
 
         for (int i = 0; i < n; i++) {
             file >> x >> y;
-            data.push_back({x,y});
+            points[i][0] = x;
+            points[i][1] = y;
         }
 
         file.close();
+        return true;
     } else {
         std::cout << "Couldn't open file";
+        return false;
     }
-
-    return data;
 }
 
-std::vector<std::pair<double, double>> findHalo(std::vector<std::pair<double, double>> data) {
+/*std::vector<std::pair<double, double>> findHalo(std::vector<std::pair<double, double>> data) {
 
 
     return data;
-}
+}*/
 
-std::pair<std::pair<double, double>, double> findNearestPoints(std::vector<std::pair<double, double>> data) {
+NearestPoints findNearestPoints(double (*points)[2], unsigned int n) {
     double minD = std::numeric_limits<double>::max();
     std::pair<double, double> point1;
     std::pair<double, double> point2;
 
-    for (int i = 0; i < data.size() - 1; i++) {
-        for (int j = i + 1; j < data.size(); j++) {
-            double x = data[j].first - data[i].first;
-            double y = data[j].second - data[i].second;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            double x = points[j][0] - points[i][0];
+            double y = points[j][1] - points[i][1];
 
             double d = sqrt((x * x) + (y * y));
             if (d < minD) {
                 minD = d;
-                point1 = data[i];
-                point2 = data[j];
+                point1 = {points[i][0], points[i][1] };
+                point2 = {points[j][0], points[j][1] };
             }
         }
     }
 
     std::cout << "Najblizsze punkty: [(" << point1.first << "," << point1.second << "),(" << point2.first << "," << point2.second << ")]" << ", d = " <<  minD << std::endl;
 
-    return make_pair(point1, minD);
+    return {point1, point2};
 }
